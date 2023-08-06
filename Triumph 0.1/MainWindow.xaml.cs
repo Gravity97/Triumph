a4a87dd60b1a85b286372f1f2c073e4189e2e8b6
Gravity <12385512+gravity98@user.noreply.gitee.com>
2023-08-06 22:10:11 +0800

    finish build env

--- Triumph 0.1/MainWindow.xaml.cs
+++ Triumph 0.1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Triumph_0._1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
